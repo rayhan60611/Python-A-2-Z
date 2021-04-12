14e8a845b79cc6949ccba58fde9172c9931a1561
M M Rayhan Parvez <pecon.alok@gmail.com>
2021-04-12 17:56:53 +0600

    Chapter 1 module Comment remove

--- 1.Chapter 1/03_pr_03_module_usage.py
+++ 1.Chapter 1/03_pr_03_module_usage.py
@@ -1,4 +1,4 @@
-# from playsound import playsound
-# playsound('H:\\Python-A-2-Z\\1.Chapter 1\\play.mp3')
+from playsound import playsound
+playsound('H:\\Python-A-2-Z\\1.Chapter 1\\play.mp3')
 
 print("hello")
