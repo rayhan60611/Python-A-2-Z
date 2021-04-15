2f949d80169f453e9882d4544219ba2064ad725d
M M Rayhan Parvez <pecon.alok@gmail.com>
2021-04-15 19:36:36 +0600

    Chapter 3 (Strings)

--- 3.Chapter-3/01_strings.py
+++ 3.Chapter-3/01_strings.py
@@ -1,4 +1,4 @@
-# b = "Rayhan's" # --> Use this if you have single quotes in your strings
+# b = "Rayhan Parvez's" # --> Use this if you have single quotes in your strings
 # b = 'Rayhan"s'
 b = '''Rayhan"s and 
        Rayhan's'''
