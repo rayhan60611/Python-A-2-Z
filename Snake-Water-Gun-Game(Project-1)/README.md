9547d5df320f8bc35ed48e0a665a5cb20d1f4661
M M Rayhan Parvez <pecon.alok@gmail.com>
2021-04-17 19:36:32 +0600

    Snake-Water-Gun-Game(Project-1)

---
+++ Snake-Water-Gun-Game(Project-1)/README.md
@@ -0,0 +1,40 @@
+# Snake Water Gun Game Using Python
+
+Snake Water Gun is one of the famous two-player game played by many people. It is a hand game in which the player randomly chooses any of the three forms i.e. snake, water, and gun. Here, we are going to implement this game using python. 
+
+## This python project is to build a game for a single player that plays with the computer.
+
+### Following are the rules of the game:
+
+1. Snake vs. Water: Snake drinks the water hence wins.
+2. Water vs. Gun: The gun will drown in water, hence a point for water
+3. Gun vs. Snake: Gun will kill the snake and win.
+4. In situations where both players choose the same object, the result will be a draw.
+
+** We will use random.randint() method and nested if-else statements to select a random item from a list. **
+
+
+### Playre Input :
+Players Turn:
+            1.Press "S" For Snake 
+            2.Press "W" For Water 
+            3.Press "G" for Gun   
+            >>>>>>>>>>>>> w  
+
+** W Means Player choose Water **
+
+
+### Computer Will give a random Object Among Water, Snake Or Gun.
+** Let's Assume Computer Has Given Gun **
+
+
+### Output:
+            Player Choose: Water!
+            Computer Choose: Gun->
+            Player Win !!!
+
+
+*Thanks For Reading*
+
+
+
---
+++ Snake-Water-Gun-Game(Project-1)/Snake-Water-Gun.py
@@ -0,0 +1,81 @@
+import random
+
+# compValue = random.randint(1, 3)
+
+
+def PlayerinputFuncton():
+    playerValue=input(f'''Players Turn:
+            1.Press "S" For Snake 
+            2.Press "W" For Water
+            3.Press "G" for Gun
+            >>>>>>>>>>>>> ''')
+    compValue = random.randint(1, 3)
+    return playerValue , compValue
+
+playerValue,compValue = PlayerinputFuncton()
+# compValue = random.randint(1, 3)
+# set Snake =1 , Water= 2 & gun = 3             
+if playerValue == "s" or playerValue == "S":
+    playerValue = 1
+elif playerValue == "w" or playerValue == "W":
+    playerValue = 2
+elif playerValue == "g" or playerValue == "G":
+    playerValue = 3
+else:
+    print("Please Give A valid input Between S, W, G")
+    PlayerinputFuncton()
+
+def game(c,p):
+    if c == p: 
+        if c==1:
+            print(f'''              
+                    Player Choose: Snake@
+                    Computer Choose: Snake@
+                    Result Is a Tie !!!''')
+        elif c==2:
+            print(f'''  
+                    Player Choose: Water!
+                    Computer Choose: Water!
+                    Result Is a Tie !!!''')
+        else:
+            print(f'''  
+                    Player Choose: Gun->
+                    Computer Choose: Gun->
+                    Result Is a Tie !!!''')
+
+    elif c == 1 and p == 2:
+        print(f'''  
+                    Player Choose: Water!
+                    Computer Choose: Snake@
+                    Computer Win !!!''')
+    elif c == 1 and p == 3:
+        print(f'''  
+                    Player Choose: Gun->
+                    Computer Choose: Snake@
+                    Player Win !!!''')
+    elif c == 2 and p == 1:
+        print(f'''  
+                    Player Choose: Snake@
+                    Computer Choose: Water!
+                    Player Win !!!''')
+    elif c == 2 and p == 3:
+        print(f'''  
+                    Player Choose: Gun->
+                    Computer Choose: Water!
+                    Computer Win !!!''')
+    elif c == 3 and p == 1:
+        print(f'''     
+                    Player Choose: Snake@
+                    Computer Choose: Gun->
+                    Computer Win !!!''')
+    elif c == 3 and p == 2:
+        print(f'''  
+                    Player Choose: Water!
+                    Computer Choose: Gun->
+                    Player Win !!!''')
+    else:
+        print("Please Give A valid input Between S, W, G")
+        PlayerinputFuncton()
+
+
+game(compValue,playerValue)
